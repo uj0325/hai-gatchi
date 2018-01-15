034ce0bf67d80bb75cb2bea1aec7bcbb2665906e
宮田雄介 <ysk379.skrmj@gmail.com>
2018-01-15 15:36:36 +0800

    プッシュ通知導入テスト

--- YUSUKE1/TOP/chat.php
+++ YUSUKE1/TOP/chat.php
@@ -15,20 +15,33 @@ var_dump($_GET);
 <head>
 	<meta charset="utf-8">
 	<title>チャット</title>
+    <!-- ========jQuery======== -->
+    <script src="../jQuery/jquery-3.1.1.js"</script>
+    <script src="../jQuery/jquery-migrate-1.4.1.js"></script>
+    <!-- ========push.js======== -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/0.0.11/push.min.js">Push.Permission.request();</script>
 </head>
 <body>
+
+<button id="push">Push</button>
 <script type="text/javascript">
-    Push.create('こんにちは！', {
-        body: '更新をお知らせします！',
-        icon: 'icon.png',
-        link: 'chat.php',
-        timeout: 8000, // 通知が消えるタイミング
-        vibrate: [100, 100, 100], // モバイル端末でのバイブレーション秒数
-        onClick: function(){
-        // 通知がクリックされた場合の設定
-        console.log(this);
-    }
-});
+  // jQueryを使うときの作法(おまじない、と称されるもの)
+    $(document).ready(function(){
+      // HTMLのコンテンツを全て読み込んだ後に実行する
+
+        $('#push').click(function(){
+            Push.create('こんにちは！', {
+            body: '更新をお知らせします！',
+            icon: 'icon.png',
+            timeout: 8000, // 通知が消えるタイミング
+            vibrate: [100, 100, 100], // モバイル端末でのバイブレーション秒数
+                onClick: function() {
+                // 通知がクリックされた場合の設定
+                console.log(this);
+                }
+            });
+        });
+    });
 </script>
 </body>
 </html>
