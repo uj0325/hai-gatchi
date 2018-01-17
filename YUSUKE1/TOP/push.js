ad8be65f0a828133ecb6766d500c3d02da620ec5
宮田雄介 <ysk379.skrmj@gmail.com>
2018-01-17 11:05:52 +0800

    commit

---
+++ YUSUKE1/TOP/push.js
@@ -0,0 +1,19 @@
+
+$(function(){
+
+    $('#push').(this).
+    on('click',function(){
+        $.ajax({
+            type:"POST",
+            url:"alert.php",
+            data:{
+                "pushed_user": login_id,
+                "receive_user": 1,
+            }
+        })
+        .done(function(data){
+           alert('通知を送信しました');
+
+        })
+    });
+});
